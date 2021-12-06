c7af177d797854436a75b7b9738adf1037b26eec
trimagnus <tri_magnus@hotmail.com>
2021-12-05 18:15:27 -0800

    Extracted css in prod, moved config rules around.

--- webpack.common.js
+++ webpack.common.js
@@ -1,21 +1,7 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  output: {
-    filename: 'main.[hash].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      }
-    ],
-  },
   plugins: [new HtmlWebpackPlugin({
     template: './src/template.html',
   })],
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,24 @@
+const path = require('path');
 const common = require('./webpack.common');
 const {merge} = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    filename: 'main.[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  plugins: [new MiniCssExtractPlugin({
+    filename: '[name].[hash].css',
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ],
+  },
 });
