d3aed5dda97345f149ca099a8e25a948f1cf0969
trimagnus <tri_magnus@hotmail.com>
2022-01-13 09:47:51 -0800

    Added an event controller

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 import './main.css';
+
+const app = document.getElementById('content');
+
+let eventController = (function() {
+  let _listeners = {};
+
+  let addListener = (actionName, callBack) => {
+    _listeners[actionName] = callBack;
+  };
+
+  let handleClick = (event) => {
+    let listeners = _listeners;
+    let action = event.target.getAttribute('data-action');
+
+    if (_listeners[action]) {
+      _listeners[action](event);
+    }
+  };
+
+  return {
+    addListener,
+    handleClick
+  };
+})();
+
+document.documentElement.addEventListener('click', eventController.handleClick, false);
